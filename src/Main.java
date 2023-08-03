7d9e802876ed74f9ca129a2d764e916c6871e00c
baizhanovsanzhar <baizhanovsanzhar@gmail.com>
2023-08-02 20:13:34 -0400

    commit3

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git03");
         System.out.println("git hub is connected");
+        System.out.println("new feature - add to local repo and git hub");
     }
 }
